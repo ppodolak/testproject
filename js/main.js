082b04e84d90a19026bd63564489f94526d4727a
Paris <serhii.tymoshchuk@icloud.com>
2025-09-23 17:00:16 +0200

    Revert "modify js.main"
    This reverts commit 4dde056cdfebf51a289fd97be914ad10cb8bd5ac.

--- js/main.js
+++ js/main.js
@@ -27,12 +27,10 @@ $(document).ready(function () {
           $("#obraz").html("<img src='/my-photo.jpg' />")
         })
         .always(function () {
-          console.log("ja się zawsze dokladnie wykonam bez względu na to czy sukces czy bląd")
+          console.log("ja się zawsze wykonam bez względu czy sukces czy bląd")
         })
 
     }
-
-
 
     $("#pokaż-obraz").on("click", function () {
       loadimage();
